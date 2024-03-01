41004746a44432ad7975f5cb32e7b3fc8e2dc4c4
Filipfill123 <FilipPolak@seznam.cz>
2024-03-01 10:53:40 +0100

    Honza version

--- servirka.py
+++ servirka.py
@@ -5,37 +5,86 @@ import logging
 from pprint import pprint, pformat
 from collections import Counter
 
-class Mocniny(Dialog):
+class ServirkaKarel(Dialog):
+    def spoj_seznam(self, polozky, spojka):
+        if len(polozky) == 0:
+            return ""
+        elif len(polozky) == 1:
+            return polozky[0]
+        else:
+            return ", ".join(polozky[:-1])+" "+spojka+" "+polozky[-1]
 
     async def main(self):
-        HLAS = "Iva30"
-        dict_of_numbers = {"nula": 0, "jedna":1, "dva": 2, "tři": 3, "čtyři": 4, "pět": 5, "šest": 6, "sedm": 7, "osm": 8, "devět": 9, "deset": 10}
+        HLAS = "Katerina210"
 
-        await self.synthesize_and_wait(text="Dobrý den. Zadejte číslo od jedné do deseti, nebo zmáčkněte tlačítko ASR a řekněte číslo.", voice=HLAS)
+        CENIK = {
+            "pivo": 49,
+            "whisky": 89,
+            "rum": 65,
+            "víno": 60,
+        }
+
+        UCET = Counter()
+
+        await self.synthesize_and_wait(text="Dobrý den, jsem vaše virtuální servírka Karel. Až si budete něco přát, stiskněte tlačítko", voice=HLAS)
         while True:
             #self.sc.led_breath_slow()
-            message = await self.sc.dm_send_message()
-            data = message["data"]
-            if data:
-                result = data["result"]
-                if result in dict_of_numbers.keys():
-                    number = dict_of_numbers[result]
-                    number_squared = str(number*number)
-                    await self.synthesize_and_wait(text=f'Druhá mocnina čísla {number} je {number_squared}', voice=HLAS)
-                    #logging.info(msg=number)
-                elif int(result) in dict_of_numbers.values():
-                    number = int(result)
-                    number_squared = str(number*number)
-                    await self.synthesize_and_wait(text=f'Druhá mocnina čísla {number} je {number_squared}', voice=HLAS)
-                else:
-                    await self.synthesize_and_wait(text=f'Omluvám se, asi jsem Vám nerozuměla.', voice=HLAS)
-
-            else: 
-                await self.synthesize_and_wait(text="Slyším a poslouchám.", voice=HLAS)
+            await self.sc.dm_send_message()
             #self.sc.led_off()
+            result = await self.synthesize_and_wait_for_asr_result(text="Přejete si prosím?", voice=HLAS, timeout=10)
 
+            while result is None:
+                logging.info("Žádný výsledek nerozpoznán")
+                result = await self.synthesize_and_wait_for_asr_result(text="Říkal jste něco?", voice=HLAS, timeout=10)
+
+            rozpoznano = result["result"]
+            logging.info(f"Rozpoznáno: {rozpoznano}")
+
+            if "na_shledanou" in rozpoznano:
+                if UCET:
+                    await self.synthesize_and_wait(text="Moment, moment, ještě jste nezaplatil!", voice=HLAS)
+                else:
+                    await self.synthesize_and_wait(text="Děkujeme, nashledanou!", voice=HLAS)
+                    break
+            elif "plat" in rozpoznano:
+                if UCET:
+                    shrnuti = []
+                    cena = 0
+                    for polozka, pocet in UCET.items():
+                        if pocet == 1:
+                            shrnuti.append(polozka)
+                        else:
+                            shrnuti.append(f"{pocet} krát {polozka}")
+                        cena += pocet * CENIK[polozka]
+
+                    shrnuti = "Měl jste: " + self.spoj_seznam(shrnuti, "a") + f". To dělá {cena} korun."
+                    await self.synthesize_and_wait(text=shrnuti, voice=HLAS)
+                    UCET.clear()
+                else:
+                    nabidka = list(CENIK.keys())
+                    nabdika = self.spoj_seznam(nabidka, "nebo")
+                    await self.synthesize_and_wait(text=f"Ještě jste si nic neobjednal. Máme třeba {nabidka}.", voice=HLAS)
+            else:
+                pocty = Counter()
+                for slovo in rozpoznano.split():
+                    if slovo in CENIK:
+                        UCET[slovo] += 1
+                        pocty[slovo] += 1
+
+                if not pocty:
+                    await self.synthesize_and_wait(text="Úplně jsem vám nerozuměla, asi už byste měl jít domů.", voice=HLAS)
+                else:
+                    shrnuti = []
+                    for polozka, pocet in pocty.items():
+                        if pocet == 1:
+                            shrnuti.append(polozka)
+                        else:
+                            shrnuti.append(f"{pocet} krát {polozka}")
+
+                    shrnuti = "Objednáme "+ self.spoj_seznam(shrnuti, "a") + f"."
+                    await self.synthesize_and_wait(text=shrnuti, voice=HLAS)
 
 if __name__ == '__main__':
     logging.basicConfig(format='%(asctime)s %(levelname)-10s %(message)s', level=logging.DEBUG)
 
-    SpeechCloudWS.run(Mocniny, address="0.0.0.0", port=8888, static_path="./static", static_route="/(.*)")
+    SpeechCloudWS.run(ServirkaKarel, address="0.0.0.0", port=8888, static_path="./static", static_route="/(.*)")
